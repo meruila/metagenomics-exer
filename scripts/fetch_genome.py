ef4bc67ffc59535ebca53e489d033a798dfb27fa
Mirella Arguelles <marguelles@up.edu.ph>
2022-04-09 21:04:26 +0800

    edited fetch_genome.py to fit with python3

---
+++ scripts/fetch_genome.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from urllib.request import urlopen
+import os
+import sys
+import time
+
+if len(sys.argv) != 3:
+    print("USAGE: fetch_genome.py <genome_id_list> <out_dir>")
+    sys.exit(1)
+
+url_template = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=%s&rettype=fasta&retmode=text"
+
+os.mkdir(sys.argv[2])
+
+for id in open(sys.argv[1]):
+    id = id.strip()
+    if id == "":
+        continue
+
+    sys.stdout.write("Fetching %s..." % id)
+    sys.stdout.flush()
+    gbk_out_file = os.path.join(sys.argv[2], id + ".fa")
+    if os.path.exists(gbk_out_file):
+        print("already fetched")
+    url = urlopen(url_template % id).read()
+    # print(url)
+    open(gbk_out_file, "wb").write(url)
+    print("Done")
+    time.sleep(1.0/3)
